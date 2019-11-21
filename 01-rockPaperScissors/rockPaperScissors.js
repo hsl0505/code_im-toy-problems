df83cc276482c05542c8d30b38ff4a8bf55d2279
hyunsung <hslgood@gmail.com>
2019-11-21 10:19:07 +0900

    보완 필요

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,44 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function (value) {
   // TODO: your solution here
+  
+  
+  
+  
+  let result = [];
+  
+  return result;
+  // math random을 써보려고 했지만... 이건 너무 비효율적인 방식이다... 
+  /*
+  let rps = ["rock", "paper", "scissors"];
+  
+  let result = [];
+
+  function maker(value) {
+    let arr = [];
+    rps.sort(function(a,b) {
+      return Math.random() - 0.5;
+    })
+
+    for (let i=0; i<value; i=i+1) {
+      arr.push(rps[0])
+    }
+
+    if (result.length !== Math.pow(value, 3)) {
+      if (!(result.includes(arr))) {
+        result.push(arr)
+        maker(value)
+      }
+      else {
+        maker(value)
+      }
+    }
+    
+  }
+  maker(value)
+  
+  return result;
+  */
 };
