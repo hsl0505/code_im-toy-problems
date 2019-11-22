c8ac636b39c6085edae4e6e7807159db63dc1e32
hyunsung <hslgood@gmail.com>
2019-11-22 09:35:33 +0900

    toy02 complete

--- 02-nonrepeatedCharacter/nonrepeatedCharacter.js
+++ 02-nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,41 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  if (string === '') {
+    return null
+  }
+
+  let temp = string.split('');
+  
+  function recursion(target) {
+    let first = target.splice(0,1)[0];
+    
+    for (let i=0; i<target.length; i=i+1) {
+      if (first === target[i]) {
+        target[i] = null;
+      }
+    }
+    
+    if (!(target.includes(null))) {
+      return first
+    }
+
+    let newArr = [];
+    for (let i=0; i<target.length; i=i+1) {
+      if (target[i] !== null) {
+        newArr.push(target[i])
+      }
+    }
+
+    if (newArr.length === 0) {
+      return null
+    }
+    
+    let newtarget = newArr;
+    return recursion(newtarget)
+  }
+  
+  return recursion(temp)
+
+  
 };
