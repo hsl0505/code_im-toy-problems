6e816a3ec0fae4c755580814bf2d9fad173cfdaf
hyunsung <hslgood@gmail.com>
2019-12-10 01:51:03 +0900

    toy11 complete

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -6,11 +6,48 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  let getParagraph = document.querySelectorAll('p');
+  
+  let splitParagraphArr = [];
+  for(let i=0; i<getParagraph.length; i++) {
+    let temp = [];
+    let splitParagraph = getParagraph[i].textContent.split(' ');
+  
+    for(let j=0; j<splitParagraph.length; j++) {
+      if(splitParagraph[j] !== "\n" && splitParagraph[j] !== "") {
+        if (splitParagraph[j].includes("\n")) {
+          let index = splitParagraph[j].indexOf("\n");
+          splitParagraph[j] = splitParagraph[j].slice(0,index);
+          temp.push(splitParagraph[j])
+        }
+        else {
+          temp.push(splitParagraph[j]);
+        }
+      }
+    }
+    splitParagraphArr.push(temp);
+  }
+  
+
+  // spanMaker.textContent = "hi"
+  // getParagraph[0].appendChild(spanMaker);
+  for(let i=0; i<splitParagraphArr.length; i++) {
+    getParagraph[i].textContent = ""
+    // console.log(splitParagraphArr[0][1])
+    for (let j=0; j<splitParagraphArr[i].length; j++) {
+      let spanMaker = document.createElement("span");
+      spanMaker.textContent = splitParagraphArr[i][j];
+      getParagraph[i].appendChild(spanMaker);
+    }
+  }
+
 
 
   // --------------STEP 2--------------
   // Next, change spans to random colors, once per second
 
   // TODO: your code here!
-
+  function changeColor() {
+    
+  }
 });
--- 11-rotatedArraySearch/rotatedArraySearch.js
+++ 11-rotatedArraySearch/rotatedArraySearch.js
@@ -15,7 +15,34 @@
  * Target time complexity: O(log(array.length))
  */
 
-var rotatedArraySearch = function (rotated, target) {
+var rotatedArraySearch = function (rotated, target) {  // 3,4,5,6,0,1,2       6,0,1,2,3,4,5
   // Your code here:
+  if (rotated[0] === target) {
+    return 0
+  }
+  else {
+    let dif = rotated[0] - target; 
+
+    if (Math.abs(dif) > rotated.length) {
+      return null
+    }
+    
+    if (dif > 0) { // 1 -> 2가나옴
+      if (rotated[0] < rotated[1]) {
+        return rotated.length-dif
+      }
+      else {  // 1 -> 5가 나옴
+        return rotated.length-dif
+      }
+    }
+    else if (dif < 0) { // 6 -> -3
+      if (rotated[0] < rotated[1]) {
+        return -(dif)
+      }
+      else {
+        return null
+      }
+    }
+  }
 };
 
