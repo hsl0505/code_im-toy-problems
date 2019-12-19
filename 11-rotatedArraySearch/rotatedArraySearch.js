60acbbad43215a0dc39d1f4cb7e3d7df88a34c53
hyunsung <hslgood@gmail.com>
2019-12-19 16:36:10 +0900

    toy12 clear

--- 11-rotatedArraySearch/rotatedArraySearch.js
+++ 11-rotatedArraySearch/rotatedArraySearch.js
@@ -15,29 +15,34 @@
  * Target time complexity: O(log(array.length))
  */
 
-var rotatedArraySearch = function (rotated, target) {  // 3,4,5,6,0,1,2       6,0,1,2,3,4,5
+var rotatedArraySearch = function(rotated, target) {
+  // 목표 시간복잡도 -> O(log n)
   // Your code here:
+  // rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2) === 5
+  // 일단 로테이트되면 항상 맨 뒷놈보다 맨 앞놈이 클 수밖에 없다 (순서대로니까)
   if (rotated[0] === target) {
-    return 0
-  }
-  else {
-    let dif = rotated[0] - target; 
+    // 맨 앞놈이 타겟과 같으면 맨 앞 인덱스 리턴
+    return 0;
+  } else {
+    let dif = rotated[0] - target; // dif -> 2 -> 원래 로테이트 전 배열(0,1,"2",3,"4",5,6)에서 타겟값과 !!!!@인덱스 차이@!!!!!
 
     if (Math.abs(dif) > rotated.length) {
-      return null
+      // 인덱스 차이가 전체 배열 길이보다 길면 target 존재 하지 않음
+      return null;
     }
-    
-    if (dif > 0) { // 1 -> 2가나옴
-      return rotated.length-dif
-    }
-    else if (dif < 0) { // 6 -> -3
+
+    if (dif > 0) {
+      // 양수이면 -> 로테이트된 배열이 아닌 구간(0,1,2,3) 에 존재한다 -> 즉, 가상의 0,1,2,3이 첫번째 ele 앞에 있다고 보고 앞으로 두번 이동 인덱스 0->-2 (-dif) + 전체 갯수만큼 이동(+7) -> 실제 인덱스
+      return rotated.length - dif; // 7-2 = 5, (6-2+1)
+    } else if (dif < 0) {
+      // 음수이면 -> 로테이트된 배열 구간에 존재 한다 (4,5,6) -> 즉, 0에서 차이만큼 이동 0-> ~
       if (rotated[0] < rotated[1]) {
-        return -(dif)
-      }
-      else {
-        return null
+        // 로테이트된 갯수가 1개 이상임
+        return -dif;
+      } else {
+        // 로테이트가 1개만 됬다 -> 즉, 로테이트된 배열의 맨 앞이 가장 큰 숫자임 -> 근데 target의 값이 더크다 (dif가 음수이다)
+        return null; // -> 타겟 값 존재 하지 않음
       }
     }
   }
 };
-
--- 12-powerSet/powerSet.js
+++ 12-powerSet/powerSet.js
@@ -7,16 +7,132 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str){
-    //
-}
+var powerSet = function(string) {
+  // 중복 제거
+  let strResult = [];
+  function deleteSame(string) {
+    let splitStr = string.split("");
+    // let result = [];
+    let first = splitStr[0];
+    strResult.push(first);
+    let temp = splitStr.slice(1);
+    if (temp.length !== 0) {
+      let newTemp = temp.filter(ele => ele !== first);
+      if (newTemp.length !== 0) {
+        return deleteSame(newTemp.join(""));
+      } else {
+        return strResult.join("");
+      }
+    } else {
+      return strResult.join("");
+    }
+  }
+
+  let newString = deleteSame(string);
+
+  // 멱집합을 찾아라
+  let result = [""];
+  let count = newString.length;
+  let str = newString.split("");
+
+  function recursion(num, arr, target, depth) {
+    for (let i = 0; i < target.length; i++) {
+      arr[depth - num] = target[i];
+      result.push(arr.slice().join(""));
+
+      let newCount = num - 1;
+
+      if (newCount !== 0) {
+        let newTarget = target.slice(target.indexOf(target[i]) + 1);
+        if (newTarget.length !== 0) {
+          recursion(newCount, arr.slice(), newTarget, depth);
+        }
+      }
+    }
+  }
+
+  recursion(count, [], str, count);
+  return result;
+};
+// function recursion(str, count) {
+//   if (count === 0) {
+//     result.push("");
+//     let newCount = count + 1;
+//     recursion(str, newCount);
+//   } else {
+//     // 카운트가 1일 때, 카운트 만큼 for루프? // 카운트가 2일 때 .........
+//     let init = "";
+//     forloop(init, str, count, deleteSame);
+//     // for (let i = 0; i < str.length; i++) {
+//     //   init = init + str[i];
+//     //   if (init.length === count && deleteSame(init)) {
+//     //     result.push(init);
+//     //     init = "";
+//     //   } else {
+//     //     for (let i = 0; i < str.length; i++) {
+//     //       init = init + str[i];
+//     //       if (init.length === count && deleteSame(init)) {
+//     //         result.push(init);
+//     //         init = "";
+//     //       }
+//     //     }
+//     //   }
+//     if (count !== str.length) {
+//       let newCount = count + 1;
+//       recursion(str, newCount);
+//     }
+//   }
+// }
+
+// // 중복제거 헬퍼함수
+// function deleteSame(target) {
+//   if (target.length === 1) {
+//     return true;
+//   }
+//   let first = target[0];
+//   for (let i = 1; i < target.length; i++) {
+//     if (target[i] === first) {
+//       return false;
+//     }
+//   }
+//   let newTarget = target.slice(1);
+//   if (newTarget.length === 1) {
+//     return true;
+//   } else {
+//     return deleteSame(newTarget);
+//   }
+// }
+
+// // for루프 돌기 헬퍼함수
+// function forloop(inittarget, str, count, cb) {
+//   console.log(result);
+//   let newinit = inittarget;
+//   for (let i = 0; i < str.length; i++) {
+//     console.log(newinit);
+//     debugger;
+//     newinit = newinit + str[i];
+//     if (newinit.length === count) {
+//       if (cb(newinit)) {
+//         result.push(newinit);
+//         newinit = inittarget;
+//       } else {
+//         newinit = inittarget;
+//       }
+//     } else {
+//       forloop(newinit, str, count, cb);
+//     }
+//   }
+//   return;
+// }
+
+// recursion(str, count);
