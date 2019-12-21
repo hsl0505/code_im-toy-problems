5863aec89d5c5d6a835facf95ce8f1d5407c66a6
hyunsung <hslgood@gmail.com>
2019-12-22 02:54:10 +0900

    toy19 clear

--- 19-longestRun/longestRun.js
+++ 19-longestRun/longestRun.js
@@ -12,17 +12,56 @@
  * inputs well.
  */
 
-var longestRun = function (string) {
+var longestRun = function(string) {
   // TODO: Your code here!
+  if (string === "") {
+    return [0, 0];
+  }
+
+  let arr = [];
+
+  let obj = {
+    char: "",
+    count: 0
+  };
+
+  for (let i = 0; i < string.length; i++) {
+    obj.char = string[i];
+    obj.count = obj.count + 1;
+    if (string[i] !== string[i + 1]) {
+      arr.push(obj);
+      obj = {
+        char: "",
+        count: 0
+      };
+    }
+  }
+
+  let nums = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    nums.push(arr[i]["count"]);
+  }
+
+  let maxNum = Math.max.apply(null, nums);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]["count"] === maxNum) {
+      return [
+        string.indexOf(arr[i]["char"]),
+        string.lastIndexOf(arr[i]["char"])
+      ];
+    }
+  }
 };
 
 // If you need a random string generator, use this!
 // (you wont need this function for your solution but it may help with testing)
-var randomString = function (len) {
+var randomString = function(len) {
   var text = "";
   var possible = "abcdefghijklmnopqrstuvwxyz";
 
-  for(var i = 0; i < len; i++) {
+  for (var i = 0; i < len; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
 
