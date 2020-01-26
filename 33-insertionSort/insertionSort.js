cac0e23fd28a29fcd8dfb18862c1f6a0f1a5a049
hyunsung <hslgood@gmail.com>
2020-01-26 17:33:10 +0900

    insertionSort summary

--- 33-insertionSort/insertionSort.js
+++ 33-insertionSort/insertionSort.js
@@ -49,31 +49,45 @@ var testingTransform = function(array) {
 var insertionSort = function(array, callback) {
   // Your code goes here. Feel free to add helper functions if needed.
   // 삽입정렬
+  // 시간복잡도 : avg O(n^2) / worst O(n^2) / best O(n)
+  // 최선의 경우는 이미 정렬이 되어있을 때
+  // -> for문 안에 for문에 안들어감(continue를 통해 건너 뜀) -> 외부 for문만 해서 O(n)이 나옴!!
 
   if (callback) {
     for (let i = 1; i < array.length; i++) {
-      for (let j = i - 1; j >= 0; j--) {
-        let compareTarget = array[j + 1];
-        // sort에서 콜백함수 결과가 -1이면 (음수) 앞이 크고, 1이면 뒤(양수)가 크다(오름차순)
-        // 이게 디폴트 (즉, a-b<0이면 정렬이 안일어나고, a-b>0이면 정렬이 일어남)
-        // a-b를 리턴하면 오름차순(디폴트), b-a를 리턴하면 내림차순 정렬(결과를 반대로)
-        // a-b>0 이면 정렬이 안일어나고, a-b<0이면 정렬이 일어아냐함
-        // 테스트는 내림차순(b-a)이므로 b-a>0일 때 정렬이 일어나도록 해야함!!
-        if (callback(array[j], compareTarget) > 0) {
-          let temp = array[j];
-          array[j] = compareTarget;
-          array[j + 1] = temp;
+      if (callback(array[i - 1], array[i]) < 0) {
+        continue;
+      } else {
+        for (let j = i - 1; j >= 0; j--) {
+          let compareTarget = array[j + 1];
+          // sort에서 콜백함수 결과가 -1이면 (음수) 앞이 크고, 1이면 뒤(양수)가 크다(오름차순)
+          // 이게 디폴트 (즉, a-b<0이면 정렬이 안일어나고, "a-b>0이면 정렬이 일어남"@@@@@@)
+          // a-b를 리턴하면 오름차순(디폴트), b-a를 리턴하면 내림차순 정렬(결과를 반대로)
+          // ------------------------------------------------------------------------------------
+          // 여기에서의 경우에는 내림차순 이므로 (결과가 반대이므로)
+          // a-b>0 이면 정렬이 안일어나고, a-b<0이면 정렬이 일어아냐함
+          // 테스트는 내림차순(b-a)이므로 b-a>0일 때 정렬이 일어나도록 해야함!!
+          if (callback(array[j], compareTarget) > 0) {
+            let temp = array[j];
+            array[j] = compareTarget;
+            array[j + 1] = temp;
+          }
         }
       }
     }
   } else {
+    // 디폴트(오름차순)
     for (let i = 1; i < array.length; i++) {
-      for (let j = i - 1; j >= 0; j--) {
-        let compareTarget = array[j + 1]; // 비교타겟은 i와 같음
-        if (array[j].value > compareTarget.value) {
-          let temp = array[j];
-          array[j] = compareTarget;
-          array[j + 1] = temp;
+      if (array[i - 1].value < array[i].value) {
+        continue;
+      } else {
+        for (let j = i - 1; j >= 0; j--) {
+          let compareTarget = array[j + 1]; // 비교타겟은 i와 같음
+          if (array[j].value > compareTarget.value) {
+            let temp = array[j];
+            array[j] = compareTarget;
+            array[j + 1] = temp;
+          }
         }
       }
     }
